8eedfab4322c8f35b36719d49831656db8c4f98b
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-04-12 22:41:27 +0200

    Create actions.pp

---
+++ manifests/actions.pp
@@ -0,0 +1,19 @@
+# == Class: curator::actions
+#
+class curator::actions {
+
+  # Configure actions.yml file using https://github.com/cataphract/puppet-yaml_settings module
+  yaml_settings { '/root/.curator/actions.yml':
+    values => $curator::actions_yml,
+  }
+  
+  # Schedule curator
+  cron { "curator_run":
+    ensure  => 'present',
+    command => '/usr/local/bin/curator --config /root/.curator/curator.yml /root/.curator/actions.yml >/dev/null',
+    hour    => 1,
+    minute  => 10,
+    weekday => '*',
+  }
+  
+}
