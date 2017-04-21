df978d3b45fa9d773608576e36c447c8e8cef6f1
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-04-21 14:38:01 +0200

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@
 class curator::config {
 
   # Configure curator.yml
-  yaml_settings { '/.root/.curator/curator.yml':
+  yaml_settings { '/root/.curator/curator.yml':
     values  => $curator::curator_yml,
   }
 
